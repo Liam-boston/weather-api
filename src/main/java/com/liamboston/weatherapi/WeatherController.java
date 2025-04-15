514c5a6cb5e650d07be0c7981139c75b67ab193e
Liam-boston <liam.boston@yahoo.com>
2025-04-15 12:40:52 -0400

    Integrated Visual Crossing Weather API

--- src/main/java/com/liamboston/weatherapi/WeatherController.java
+++ src/main/java/com/liamboston/weatherapi/WeatherController.java
@@ -1,4 +1,17 @@
 package com.liamboston.weatherapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WeatherController {
+    @Autowired
+    private WeatherService weatherService;
+
+    @GetMapping("/weather/{country}")
+    public String getWeather(@PathVariable String country) {
+        return weatherService.getWeather(country);
+    }
 }
--- src/main/java/com/liamboston/weatherapi/WeatherService.java
+++ src/main/java/com/liamboston/weatherapi/WeatherService.java
@@ -1,4 +1,22 @@
 package com.liamboston.weatherapi;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class WeatherService {
+    private static final String API_KEY = "C6RYG2CVQMRPTFH5VMRSA5FXL";
+    private static final String BASE_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+
+    public String getWeather(String country) {
+        String url = BASE_URL + country + "/?key=" + API_KEY;
+
+        // Using RestTemplate to make HTTP Request
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+        // Return response body as a String (can be parsed into a proper object later)
+        return response.getBody();
+    }
 }
