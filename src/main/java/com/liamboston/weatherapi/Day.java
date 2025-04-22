2f889be89d4384a3c8f13a2fc9b7bb28330fd343
Liam-boston <liam.boston@yahoo.com>
2025-04-22 18:21:00 -0400

    Updated date format to Month dd, yyyy

--- src/main/java/com/liamboston/weatherapi/Day.java
+++ src/main/java/com/liamboston/weatherapi/Day.java
@@ -2,6 +2,8 @@ package com.liamboston.weatherapi;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Day {
@@ -67,9 +69,23 @@ public class Day {
         this.description = description;
     }
 
+    // Method to format the datetime to "Month dd, yyyy"
+    private String formatDate(String dateStr) {
+        try {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = inputFormat.parse(dateStr);
+            SimpleDateFormat outputFormat = new SimpleDateFormat("MMMM dd, yyyy");
+            return outputFormat.format(date);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return dateStr;
+        }
+    }
+
     @Override
     public String toString() {
-        return "<b>" + datetime + "</b><br>" +
+        String formattedDate = formatDate(datetime);
+        return "<b>" + formattedDate + "</b><br>" +
                 "<b>Forecast: </b>" + description + "<br>" +
                 "<b>Low: </b>" + tempmin + "<b>° | High: </b>" + tempmax + "<b>°</b><br>" +
                 "<b>Precipitation: </b>" + (preciptype != null && !preciptype.isEmpty() ? String.join(", ", preciptype) : "None") +
